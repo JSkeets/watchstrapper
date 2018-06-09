aab2e4b14859b27bf95d66f3a807553073a2fcc5
Jesse Skeets <jesseskeets@gmail.com>
2018-06-09 16:08:58 -0700

    [watchstrapper] able to swap between two straps

--- src/App.js
+++ src/App.js
@@ -3,9 +3,32 @@ import logo from "./logo.svg";
 import speedmaster from "./Speedmaster.jpeg";
 import watchTop from "./watchstraptop.jpg";
 import watchBottom from "./WatchStrapLong.jpg";
+import watchTop2 from "./watch2Top.jpg";
+import watchBottom2 from "./strap2bottom.jpg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      top: watchTop,
+      bottom: watchBottom
+    };
+    this.swapImages = this.swapImages.bind(this);
+  }
+  swapImages() {
+    if (this.state.top === watchTop) {
+      this.setState({
+        top: watchTop2,
+        bottom: watchBottom2
+      });
+    } else {
+      this.setState({
+        top: watchTop,
+        bottom: watchBottom
+      });
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -16,16 +39,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
+        <button onClick={this.swapImages}>Swap straps</button>
         <div class="watch-container">
           <div id="top">
-            <img src={watchTop} className="watchStrap" />
+            <img src={this.state.top} className="watchStrap" />
           </div>
           <div id="watch">
             <img src={speedmaster} className="watch" />
           </div>
           <div id="bottom">
-            <img src={watchBottom} className="watchStrap" id="bottom" />
+            <img src={this.state.bottom} className="watchStrap" id="bottom" />
           </div>
         </div>
       </div>
