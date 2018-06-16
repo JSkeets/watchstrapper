ef0465083d232e573ecb67a5597c74b2948027c4
Jesse Skeets <jesseskeets@gmail.com>
2018-06-16 13:28:03 -0700

    [watchstrapper] added more straps

--- src/App.js
+++ src/App.js
@@ -15,21 +15,39 @@ class App extends Component {
       bottom: watchBottom,
       watch:
         "https://cdn.shopify.com/s/files/1/0146/0732/files/GALLET.png?4139486667208975691",
-      position: 0
+      watchPosition: 0,
+      strapPosition: 0,
+      strapTop:
+        "https://cdn.shopify.com/s/files/1/0146/0732/files/SMOOTH-B-20_13921bed-846e-4018-bbff-e93d67d3bd9d.png?9801324259905238006",
+      strapBottom:
+        "https://cdn.shopify.com/s/files/1/0146/0732/files/SMOOTH-B-20.png?9801324259905238006"
     };
     this.swapImages = this.swapImages.bind(this);
     this.swapWatches = this.swapWatches.bind(this);
   }
   swapImages() {
-    if (this.state.top === watchTop) {
+    let strapTops = [
+      "https://cdn.shopify.com/s/files/1/0146/0732/files/SMOOTH-B-20_13921bed-846e-4018-bbff-e93d67d3bd9d.png?9801324259905238006",
+      "https://cdn.shopify.com/s/files/1/0146/0732/files/CANVAS-L-BG-20_a8353ee3-0d67-474d-ad29-d2b6e25109bf.png?18426604203995489985",
+      "https://cdn.shopify.com/s/files/1/0146/0732/files/STAINED-BG-20_df901a35-1763-4f03-87c5-42103f01bd41.png?2900963067896406454"
+    ];
+    let strapBottoms = [
+      "https://cdn.shopify.com/s/files/1/0146/0732/files/SMOOTH-B-20.png?9801324259905238006",
+      "https://cdn.shopify.com/s/files/1/0146/0732/files/CANVAS-L-BG-20_a8353ee3-0d67-474d-ad29-d2b6e25109bf.png?18426604203995489985",
+      "https://cdn.shopify.com/s/files/1/0146/0732/files/STAINED-BG-20.png?2900963067896406454"
+    ];
+    let position = this.state.strapPosition;
+    if (position < 2) {
       this.setState({
-        top: watchTop2,
-        bottom: watchBottom2
+        strapBottom: strapBottoms[position + 1],
+        strapTop: strapTops[position + 1],
+        strapPosition: position + 1
       });
     } else {
       this.setState({
-        top: watchTop,
-        bottom: watchBottom
+        strapBottom: strapBottoms[0],
+        strapTop: strapTops[0],
+        strapPosition: 0
       });
     }
   }
@@ -40,16 +58,16 @@ class App extends Component {
       "https://cdn.shopify.com/s/files/1/0146/0732/files/PORTUGEUSE.png?4139486667208975691",
       "https://cdn.shopify.com/s/files/1/0146/0732/files/SUB.png?4139486667208975691"
     ];
-    let position = this.state.position;
+    let position = this.state.watchPosition;
     if (position < 2) {
       this.setState({
         watch: watches[position + 1],
-        position: position + 1
+        watchPosition: position + 1
       });
     } else {
       this.setState({
         watch: watches[0],
-        position: 0
+        watchPosition: 0
       });
     }
   }
@@ -61,10 +79,7 @@ class App extends Component {
         <div class="watch-container">
           <div id="top">
             {/* <img src={this.state.top} className="watchStrap" /> */}
-            <img
-              src="https://cdn.shopify.com/s/files/1/0146/0732/files/STAINED-BG-20_df901a35-1763-4f03-87c5-42103f01bd41.png?2900963067896406454"
-              className="watchStrap"
-            />
+            <img src={this.state.strapTop} className="watchStrap" />
           </div>
           <div id="watch">
             <img src={this.state.watch} className="watch" />
@@ -72,7 +87,7 @@ class App extends Component {
           <div id="bottom">
             {/* <img src={this.state.bottom} className="watchStrap" id="bottom" /> */}
             <img
-              src="https://cdn.shopify.com/s/files/1/0146/0732/files/STAINED-BG-20.png?2900963067896406454"
+              src={this.state.strapBottom}
               className="watchStrap"
               id="bottom"
             />
