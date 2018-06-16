b99de9ac6042fbf24c79668764864bf4995ff503
Jesse Skeets <jesseskeets@gmail.com>
2018-06-16 12:47:45 -0700

    [watchstrapper] copied code from hodinkee able to swap watches, looks much cleaner

--- src/App.js
+++ src/App.js
@@ -12,9 +12,13 @@ class App extends Component {
     super();
     this.state = {
       top: watchTop,
-      bottom: watchBottom
+      bottom: watchBottom,
+      watch:
+        "https://cdn.shopify.com/s/files/1/0146/0732/files/GALLET.png?4139486667208975691",
+      position: 0
     };
     this.swapImages = this.swapImages.bind(this);
+    this.swapWatches = this.swapWatches.bind(this);
   }
   swapImages() {
     if (this.state.top === watchTop) {
@@ -29,26 +33,49 @@ class App extends Component {
       });
     }
   }
+
+  swapWatches() {
+    let watches = [
+      "https://cdn.shopify.com/s/files/1/0146/0732/files/GALLET.png?4139486667208975691",
+      "https://cdn.shopify.com/s/files/1/0146/0732/files/PORTUGEUSE.png?4139486667208975691",
+      "https://cdn.shopify.com/s/files/1/0146/0732/files/SUB.png?4139486667208975691"
+    ];
+    let position = this.state.position;
+    if (position < 2) {
+      this.setState({
+        watch: watches[position + 1],
+        position: position + 1
+      });
+    } else {
+      this.setState({
+        watch: watches[0],
+        position: 0
+      });
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <button onClick={this.swapImages}>Swap straps</button>
+        <button onClick={this.swapWatches}>Swap wathes</button>
         <div class="watch-container">
           <div id="top">
-            <img src={this.state.top} className="watchStrap" />
+            {/* <img src={this.state.top} className="watchStrap" /> */}
+            <img
+              src="https://cdn.shopify.com/s/files/1/0146/0732/files/STAINED-BG-20_df901a35-1763-4f03-87c5-42103f01bd41.png?2900963067896406454"
+              className="watchStrap"
+            />
           </div>
           <div id="watch">
-            <img src={speedmaster} className="watch" />
+            <img src={this.state.watch} className="watch" />
           </div>
           <div id="bottom">
-            <img src={this.state.bottom} className="watchStrap" id="bottom" />
+            {/* <img src={this.state.bottom} className="watchStrap" id="bottom" /> */}
+            <img
+              src="https://cdn.shopify.com/s/files/1/0146/0732/files/STAINED-BG-20.png?2900963067896406454"
+              className="watchStrap"
+              id="bottom"
+            />
           </div>
         </div>
       </div>
